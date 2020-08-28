763a84d43a7b9b280e0a3dacffe9b00c79cef80d
fnhuang <felicia.hlj@gmail.com>
2020-08-28 15:35:44 +0800

    add tweet source

--- data_analysis.py
+++ data_analysis.py
@@ -9,7 +9,8 @@ from nltk.corpus import stopwords
 import math, statistics, string
 import numpy as np
 import heapq
-
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from googletrans import Translator
 
 FOLDER_SPEC = "folder_spec.csv"
 
@@ -101,6 +102,69 @@ class TopicAnalyzer():
 
 
         writer.close()
+
+    #this method should be run after write_theta_given_lambda
+    #topics under topics folder created under write_theta_given_lambda are manually reassigned
+    #then this method should be run to generate new theta distribution
+    def write_theta_after_manual_tinkering(self):
+        topic2tweets = {}
+        for file in os.listdir(f"{self.topic_folder}/topics/"):
+            with open(f"{self.topic_folder}/topics/{file}", "r", encoding="utf8") as reader:
+                topic = int(file.replace(".txt",""))
+
+                lines = reader.readlines()
+                tweets = [l.strip() for l in lines]
+
+                topic2tweets[topic] = tweets
+
+        writer = open(f"{self.topic_folder}/TopicsRelevanceOnUsers.txt", "w", encoding="utf8")
+
+        unexisting_tweets = []
+        with open(f"{self.topic_folder}/TopicsDistributionOnUsers.txt", "r", encoding="utf8") as reader:
+            lines = reader.readlines()
+
+            # for each file/day/document
+            doc_no = 0
+            for line in lines:
+                topic_dist = [0] * len(topic2tweets)
+
+                file = line.split("\t")[0].strip()
+                writer.write(f"{file}\t")
+                with open(f"{self.data_folder}/{file}", "r", encoding="utf8") as reader:
+                    tweets = reader.readlines()
+
+                    for tweet in tweets:
+                        tw = tweet.strip()
+                        tw_topic = -1
+
+                        for topic in topic2tweets:
+                            if tw in topic2tweets[topic]:
+                                tw_topic = topic
+                                break
+
+                        if tw_topic == -1:
+                            unexisting_tweets.append(tweet)
+                        else:
+                            topic_dist[tw_topic] += 1
+
+                # sum of topic dist = 1
+                topic_dist = [topic_dist[i] / sum(topic_dist) for i in range(0, len(topic_dist))]
+
+                topic_dist = [str(t) for t in topic_dist]
+                writer.write("\t".join(topic_dist))
+                writer.write("\n")
+                writer.flush()
+
+                doc_no += 1
+                print("\r", end="")
+                print("Processing", doc_no, "out of", len(lines), end="", flush=True)
+
+        writer.close()
+
+        print("\n")
+        print(len(unexisting_tweets))
+        for tweet in unexisting_tweets:
+            print(tweet)
 
     def write_theta_given_lambda(self, lam):
         vocabs, top2wrelv_matrix = self.get_word_relevance_in_topic(lam)
@@ -437,6 +501,42 @@ class TopicAnalyzer():
 
 
 
+class TextProcessor():
+    # remove www
+    def remove_link(self, text):
+        words = text.split(" ")
+        valid_words = []
+
+        for w in words:
+            if not w.startswith("http") and not w.startswith("https") and not w.startswith("www"):
+                valid_words.append(w)
+
+        return " ".join(valid_words)
+
+    # perform tokenization
+    def basic_preprocess(self, text):
+        # tokenize and remove stopwords
+
+        words = word_tokenize(text)
+
+        # step 1: remove stopwords
+        stop_removed = [w for w in words if w not in set(stopwords.words('english'))]
+
+        # step 2: remove punctuations
+        punct_removed = [w for w in stop_removed if w not in string.punctuation]
+
+        # step 3: remove non-alphabet
+        regex = re.compile('[^a-zA-Z]')
+        nonalph_removed = [regex.sub('', w) for w in punct_removed]
+
+        # step 4: remove empty words
+        cleaned = [w for w in nonalph_removed if w != "" and len(w) > 1]
+
+        if len(cleaned) > 0:
+            return " ".join(cleaned)
+        else:
+            return ""
+
 class SeedTweetsAnalyzer():
     def __init__(self, seed_tweets_folder):
         self.seed_tweets_folder = seed_tweets_folder
@@ -445,16 +545,18 @@ class SeedTweetsAnalyzer():
 
     def _get_rt_status_if_retweeted(self, jtweet):
         is_a_retweet = False
-        source = -1
+        user_source = -1
+        tweet_source = -1
 
         if "retweeted_status" in jtweet.keys():
             text = self._get_tweet_from_json(jtweet["retweeted_status"])
-            source = jtweet["retweeted_status"]["user"]["id_str"]
+            user_source = jtweet["retweeted_status"]["user"]["id_str"]
+            tweet_source = jtweet["retweeted_status"]["id_str"]
             is_a_retweet = True
         else:
             text = self._get_tweet_from_json(jtweet)
 
-        return is_a_retweet,source,text
+        return is_a_retweet,user_source,tweet_source,text
 
     def _get_tweet_from_json(self, json_obj):
         if "full_text" in json_obj.keys():
@@ -556,7 +658,7 @@ class SeedTweetsAnalyzer():
         ctwriter = csv.writer(tweets_file)
         cuwriter = csv.writer(users_file)
 
-        ctwriter.writerow(["status_id","date_sgtime","text","is_a_retweet","source","retweets","likes","user_id"])
+        ctwriter.writerow(["status_id","date_sgtime","text","is_a_retweet","user_source","tweet_source","retweets","likes","user_id"])
         cuwriter.writerow(["user_id","user_name","user_location","followers","followees","tweets","favourites","lists","created_at_sgtime"])
 
         with open("collected_tweets.json","r",encoding="utf8") as reader:
@@ -565,7 +667,7 @@ class SeedTweetsAnalyzer():
 
                 status_id = jtweet["id_str"]
 
-                is_a_retweet,source,text = self._get_rt_status_if_retweeted(jtweet)
+                is_a_retweet, user_source, tweet_source, text = self._get_rt_status_if_retweeted(jtweet)
 
                 created_at = datetime.strptime(jtweet["created_at"], "%a %b %d %H:%M:%S %z %Y")
                 created_at = created_at.replace(tzinfo=utc).astimezone(tz=timezone("Asia/Singapore"))
@@ -574,7 +676,8 @@ class SeedTweetsAnalyzer():
                 likes = jtweet["favorite_count"]
                 user_id = jtweet["user"]["id"]
 
-                ctwriter.writerow([status_id, date_str, text, is_a_retweet, source, retweets, likes, user_id])
+                ctwriter.writerow([status_id, date_str, text, is_a_retweet, user_source, tweet_source,
+                                   retweets, likes, user_id])
                 tweets_file.flush()
 
                 if user_id not in users:
@@ -597,41 +700,7 @@ class SeedTweetsAnalyzer():
         users_file.close()
 
 
-    # remove www
-    def _remove_link(self,text):
-        words = text.split(" ")
-        valid_words = []
 
-        for w in words:
-            if not w.startswith("http") and not w.startswith("https") and not w.startswith("www"):
-                valid_words.append(w)
-
-        return " ".join(valid_words)
-
-
-    # perform tokenization
-    def _basic_preprocess(self, text):
-        #tokenize and remove stopwords
-
-        words = word_tokenize(text)
-
-        # step 1: remove stopwords
-        stop_removed = [w for w in words if w not in set(stopwords.words('english'))]
-
-        # step 2: remove punctuations
-        punct_removed = [w for w in stop_removed if w not in string.punctuation]
-
-        # step 3: remove non-alphabet
-        regex = re.compile('[^a-zA-Z]')
-        nonalph_removed = [regex.sub('', w) for w in punct_removed]
-
-        #step 4: remove empty words
-        cleaned = [w for w in nonalph_removed if w != "" and len(w) > 1]
-
-        if len(cleaned) > 0:
-            return " ".join(cleaned)
-        else:
-            return ""
 
     def transform_filtered_tweets_as_TLDA_input(self, result_folder):
         dtweet = {}
@@ -654,8 +723,8 @@ class SeedTweetsAnalyzer():
         for datstr in dtweet.keys():
             with open(f"{result_folder}/{datstr}.txt","w", encoding="utf8") as writer:
                 for tweet in dtweet[datstr]:
-                    tweet = self._remove_link(tweet)
-                    toktweet = self._basic_preprocess(tweet)
+                    tweet = TextProcessor().remove_link(tweet)
+                    toktweet = TextProcessor().basic_preprocess(tweet)
                     if len(toktweet) > 0:
                         writer.write(f"{toktweet}\n")
                         writer.flush()
@@ -727,9 +796,16 @@ class SeedUsersAnalyzer():
 
 
 class FinalDataPrep():
-    def prepare_topic_file(self):
-        writer = open("topics_tableau.csv","w",encoding="utf8")
+    def __init__(self, final_data_folder):
+        self.final_data_folder = final_data_folder
+
+    def prepare_topic_file(self, num_topic):
+        writer = open(f"{self.final_data_folder}/topics_top3.csv","w",encoding="utf8")
         writer.write("date,first_topic,first_pct,second_topic,second_pct,third_topic,third_pct\n")
+
+        awriter = open(f"{self.final_data_folder}/topics_all.csv", "w", encoding="utf8")
+        topic_names = ",".join([str(i) for i in range(0, num_topic)])
+        awriter.write(f"date,{topic_names}\n")
 
         with open("TopicsRelevanceOnUsers.txt", "r", encoding="utf8") as reader:
             lines = reader.readlines()
@@ -740,17 +816,176 @@ class FinalDataPrep():
                 probs = []
                 for data in datas[1:]:
                     probs.append(float(data))
+                pjoin = ",".join([str(p) for p in probs])
+                awriter.write(f"{date},{pjoin}\n")
 
                 indices = np.array(probs).argsort()[-3:][::-1]
 
                 writer.write(f"{date},{indices[0]},{probs[indices[0]]},{indices[1]},"
                              f"{probs[indices[1]]},{indices[2]},{probs[indices[2]]}\n")
                 writer.flush()
+                awriter.flush()
 
         writer.close()
+        awriter.close()
 
-    '''def prepare_tweets_file(self):
-        with open("tweets.csv", )'''
+    def _count_pronouns(self, text):
+        first_person_singular = ["i","me","my","mine"]
+        first_person_plural = ["we","us","our","ours"]
+        second_person = ["you","your","yours"]
+        third_person_singular = ["he","him","his","she","her","hers","it","its"]
+        third_person_plural = ["they","them","their","theirs"]
+
+        pronouns = []
+
+        words = word_tokenize(text)
+
+        count = 0
+        for w in words:
+            if w in first_person_singular:
+                count += 1
+        pronouns.append(count)
+
+        count = 0
+        for w in words:
+            if w in first_person_plural:
+                count += 1
+        pronouns.append(count)
+
+        count = 0
+        for w in words:
+            if w in second_person:
+                count += 1
+        pronouns.append(count)
+
+        count = 0
+        for w in words:
+            if w in third_person_singular:
+                count += 1
+        pronouns.append(count)
+
+        count = 0
+        for w in words:
+            if w in third_person_plural:
+                count += 1
+        pronouns.append(count)
+
+        return pronouns
+
+    def _get_phase(self, date_sgtime):
+        dat = datetime.strptime(date_sgtime, "%Y-%m-%d %H:%M:%S")
+        imp_dat = datetime.strptime("2020-02-04 00:00:00", "%Y-%m-%d %H:%M:%S")
+        ser_dat = datetime.strptime("2020-03-13 00:00:00", "%Y-%m-%d %H:%M:%S")
+        cb_dat = datetime.strptime("2020-04-07 00:00:00", "%Y-%m-%d %H:%M:%S")
+        cbt_dat = datetime.strptime("2020-04-21 00:00:00", "%Y-%m-%d %H:%M:%S")
+        cbr_dat = datetime.strptime("2020-05-02 00:00:00", "%Y-%m-%d %H:%M:%S")
+        open_dat = datetime.strptime("2020-06-02 00:00:00", "%Y-%m-%d %H:%M:%S")
+        if dat < imp_dat:
+            phase = "imported"
+        elif dat >= imp_dat and dat < ser_dat:
+            phase = "early local clusters"
+        elif dat >= ser_dat and dat < cb_dat:
+            phase = "stronger measure"
+        elif dat >= cb_dat and dat < cbt_dat:
+            phase = "circuit breaker initial measure"
+        elif dat >= cbt_dat and dat < cbr_dat:
+            phase = "circuit breaker tightened measure"
+        elif dat >= cbr_dat and dat < open_dat:
+            phase = "circuit breaker relaxed measure"
+        else:
+            phase = "safe reopening"
+
+        return phase
+
+    def prepare_tweets_file(self):
+        analyser = SentimentIntensityAnalyzer()
+        translator = Translator()
+
+        writer = open(f"{self.final_data_folder}/tweets_otherinfo.csv","w", encoding="utf8",
+                      newline="")
+        rwriter = open(f"{self.final_data_folder}/retweets.csv","w", encoding="utf8",
+                       newline="")
+
+        csv_writer = csv.writer(writer)
+        csv_rwriter = csv.writer(rwriter)
+
+        csv_writer.writerow(["status_id","date_sgtime","date_only","phase",
+                          "text","cleaned_text","is_a_retweet","retweet_source",
+                          "likes","sentiment_score","is_positive","is_neutral",
+                          "is_negative","first_person_singular","first_person_plural",
+                          "second_person","third_person_singular","third_person_plural",
+                          "user_id"])
+        csv_rwriter.writerow(["status_id","date_sg_time","date_only","text","cleaned_text",
+                      "num_retweets","retweet_source","user_id"])
+
+        tweet_sources = []
+
+        text_length = 0
+        with open("tweets.csv", "r", encoding="utf8") as reader:
+            lines = reader.readlines()
+            text_length = len(lines)-1
+
+        with open("tweets.csv", "r", encoding="utf8") as reader:
+            csvreader = csv.DictReader(reader)
+
+            #for every tweet
+            doc_count = 0
+            for row in csvreader:
+                doc_count += 1
+
+                status_id = row["status_id"]
+
+                date_sgtime = row["date_sgtime"]
+                date_only = row["date_sgtime"].split(" ")[0]
+                phase = self._get_phase(date_sgtime)
+
+                text = row["text"]
+                cleaned_text = TextProcessor().remove_link(text)
+                cleaned_text = TextProcessor().basic_preprocess(cleaned_text)
+
+                is_a_retweet = row["is_a_retweet"]
+                source = row["source"]
+                num_retweets = row["retweets"]
+
+                user_id = row["user_id"]
+
+                if is_a_retweet == "True" and source not in tweet_sources:
+                    # only write one retweet coming from a source,
+                    # so that the value of retweets does not repeat
+                    tweet_sources.append(source)
+                    csv_rwriter.writerow([status_id, date_sgtime, date_only, text, cleaned_text,
+                                          num_retweets, source, user_id])
+                    rwriter.flush()
+
+
+                likes = row["likes"]
+
+                is_positive = 0
+                is_neutral = 0
+                is_negative = 0
+
+                teks = translator.translate(text).text
+                sentiment_score = analyser.polarity_scores(teks)['compound']
+
+                if sentiment_score >= 0.05:
+                    is_positive = 1
+                elif (sentiment_score > -0.05) and (sentiment_score < 0.05):
+                    is_neutral = 1
+                else:
+                    is_negative = 1
+
+                pronouns = self._count_pronouns(text)
+
+                csv_writer.writerow([status_id,date_sgtime,date_only,phase,text,cleaned_text,
+                                      is_a_retweet,source,likes,sentiment_score,is_positive,
+                                      is_neutral,is_negative,pronouns[0],pronouns[1],
+                                      pronouns[2],pronouns[3],pronouns[4],user_id])
+
+                print("\r", end="")
+                print("processing doc",doc_count,"out of",text_length,end="",flush=True)
+                writer.flush()
+        rwriter.close()
+        writer.close()
 
 if __name__ == "__main__":
     folder_specs = get_folder_spec()
@@ -774,6 +1009,7 @@ if __name__ == "__main__":
     #sua.collect_tweets_with_phrase("#sgunited")
 
     #ta = TopicAnalyzer("C:\\Users\\fnatali\\eclipse-workspace\\Twitter-LDA-master\\data","sgu_ntopic_25", "sgunited")
+    #ta.write_theta_after_manual_tinkering()
     #ta.write_theta_given_lambda(0.4)
     #ta.get_word_relevance_in_topic(0.4)
     #ta.analyze_coherence_score_of_files("ModelRes/sgu")
@@ -782,5 +1018,6 @@ if __name__ == "__main__":
     #ta.prepare_file_for_ldavis()
     #ta.write_top30_words()
 
-    #fdp = FinalDataPrep()
-    #fdp.prepare_topic_file()
+    #fdp = FinalDataPrep("final_data")
+    #fdp.prepare_topic_file(25)
+    #fdp.prepare_tweets_file()
